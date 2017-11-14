75215f7686767641970717e84fd09251a31d6466
瑞祥 陈 <jesse@zuishoutekiMacBook-Air.local>
2017-11-15 04:15:06 +0800

    change port

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,13 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/getCalculateResultList', function(req, res, next) {
+
+  let results = require('fs').readdirSync(__dirname + "/../service/result");
+
+  console.log(results);
+  res.append("Content-Type", 'text/html; charset=utf-8');
+  res.json(results);
+});
+
 module.exports = router;
