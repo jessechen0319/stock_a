33d0123115f3282c043dac9b27742132a374843d
瑞祥 陈 <jesse@zuishoutekiMacBook-Air.local>
2017-12-06 22:30:02 +0800

    s

--- service/fetch/fetchMethod/day144_55_向上.js
+++ service/fetch/fetchMethod/day144_55_向上.js
@@ -14,38 +14,28 @@ function calculate(data, stock) {
             //计算非布拉切均线
             data = util.generateFibonacci(data);
 
+
+            if(stock == 'sz002360'){
+                console.log('-----------');
+                console.log(data.mashData[0].ma144);
+                console.log(data.mashData[1].ma144);
+                console.log(data.mashData[0].ma55);
+                console.log(data.mashData[1].ma55);
+                console.log('-----------');
+            }
+
             if (data.mashData[0].ma144 > data.mashData[1].ma144) {
                 if (data.mashData[0].ma55 > data.mashData[1].ma55) {
-                    let results = jsonfile.readFileSync(resultJsonPath);
-                    results.push({ "stock": stock, "date": data.mashData[0].date, "price": data.mashData[0].kline.close });
-                    console.log(`insert growth 144 55${stock}`);
-                    jsonfile.writeFileSync(resultJsonPath, results);
+                    if(data.mashData[0].ma144<data.mashData[0].ma55){
+                        let results = jsonfile.readFileSync(resultJsonPath);
+                        results.push({ "stock": stock, "date": data.mashData[0].date, "price": data.mashData[0].kline.close });
+                        console.log(`insert growth 144 55${stock}`);
+                        jsonfile.writeFileSync(resultJsonPath, results);
+                    }
+                    
 
                 }
             }
-
-            // if (data.mashData[0].kline.close > data.mashData[0].ma144) {//站上所有均线
-            //     if (Math.abs(data.mashData[0].ma144 - data.mashData[5].ma144) < 0.04) {//144走平
-            //         if (data.mashData[0].kline.open < data.mashData[0].ma144) {
-            //             if (data.mashData[0].ma144 > data.mashData[0].ma55) {
-            //                 if (data.mashData[0].ma144 > data.mashData[0].ma34) {
-            //                     if (data.mashData[0].ma144 > data.mashData[0].ma13) {
-            //                         if (data.mashData[0].ma13 >= data.mashData[1].ma13) {
-            //                             if (data.mashData[0].ma34 >= data.mashData[1].ma34) {
-            //                                 let results = jsonfile.readFileSync(resultJsonPath);
-            //                                 results.push({ "stock": stock, "date": data.mashData[0].date, "price": data.mashData[0].kline.close });
-            //                                 console.log(`insert growth ${stock}`);
-            //                                 jsonfile.writeFileSync(resultJsonPath, results);
-            //                             }
-            //                         }
-            //                     }
-            //                 }
-
-            //             }
-            //         }
-            //     }
-
-            // }
         }
     } catch (error) {
         console.log(error)
