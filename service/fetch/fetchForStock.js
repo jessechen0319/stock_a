487af5432da8f0fd7551fe134f1acdb8e13496e4
xiang19890319@gmail.com <xiang19890319@gmail.com>
2017-12-22 07:29:17 +0800

    底分型

--- service/fetch/fetchForStock.js
+++ service/fetch/fetchForStock.js
@@ -126,7 +126,7 @@ let zhouxianfenxing = require('./fetchMethod/周线底分型');
 let zhouKxiadie = require('./fetchMethod/不改变趋势周K下跌');
 
 
-fetchForStock.setAnalysis([zhouKxiadie.calculate]);
+fetchForStock.setAnalysis([zhouxianfenxing.calculate]);
 
 let chainRunner = new ChainTaskRunner();
 
--- service/fetch/fetchMethod/周线底分型.js
+++ service/fetch/fetchMethod/周线底分型.js
@@ -4,18 +4,74 @@ let resultJsonPath = __dirname + '/../../result/周线底分型.json';
 const util = require('../../util/Fibonacci');
 const jsonfile = require('jsonfile');
 jsonfile.writeFileSync(resultJsonPath, []);
+
+function processCover(data){
+
+    let processedArray = [];
+
+    for(let i = 0; i < 10 ; i++){
+        processedArray.push(data.mashData[i].kline);
+    }
+
+    let cursor = 8;
+
+    while(cursor > 0){
+
+        let before = processedArray[cursor];
+        let after = processedArray[cursor-1];
+        let isCover = false;
+        let high = after.high, low = after.low;
+        if(before.high > after.high && before.low < after.low){
+            isCover = true;
+        } else if (before.high < after.high && before.low > after.low){
+            isCover = true;
+        }
+
+        if(isCover){
+            if(processedArray[cursor+1].high < after.high){
+                //上升
+                high = Math.max(before.high , after.high);
+                low = Math.max(before.low , after.low);
+            } else {
+                //下降
+                high = Math.min(before.high , after.high);
+                low = Math.min(before.low , after.low);
+            }
+            processedArray[cursor-1].high = high;
+            processedArray[cursor-1].low = low;
+            processedArray[cursor].removeable = true;
+        }
+        cursor--;
+    }
+
+    let returnValue = [];
+    processedArray.forEach((item)=>{
+        if(!item.removeable){
+            returnValue.push(item);
+        }
+    });
+    return returnValue;
+}
+
 function calculate(data, stock){
     try {
         data = JSON.parse(data);
-        console.log(data.mashData.length);
         if (data.mashData.length > 155) {
-            if(data.mashData[1].kline.high<data.mashData[2].kline.high){
-                if(data.mashData[1].kline.high<data.mashData[2].kline.high){
-                    if(data.mashData[1].kline.low<data.mashData[0].kline.low){
-                        if(data.mashData[1].kline.low<data.mashData[0].kline.low){
+
+            let processedCover = processCover(data);
+            //确定底分型
+            let right = processedCover[0];
+            let middle = processedCover[1];
+            let left = processedCover[2];
+
+            if(middle.low < right.low && middle.high < right.high){
+                if(middle.low < left.low && middle.high < left.high){
+                    let lengthOfMiddle = middle.high - middle.low;
+                    if(middle.high - middle.close < middle.close - middle.low){
+                        if(right.close*2>(left.low + left.high)){
                             let results = jsonfile.readFileSync(resultJsonPath);
                             results.push({ "stock": stock, "date": data.mashData[0].date, "price": data.mashData[0].kline.close });
-                            console.log(`insert dragon ${stock}`);
+                            console.log(`insert di weekly ${stock}`);
                             jsonfile.writeFileSync(resultJsonPath, results);
                         }
                     }
