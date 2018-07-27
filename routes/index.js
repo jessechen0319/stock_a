9989c418f32f6c701cc2f30d66591c5224dfcd54
Jesse Chen <48510049@qq.com>
2018-07-27 14:35:46 +0800

    change UI trigger

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var fetchService = require("../service/fetch/fetchForStock");
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/getCalculateResultList', function(req, res, next) {
+router.get('/trigger', function(req, res, next) {
 
-  let results = require('fs').readdirSync(__dirname + "/../service/result");
+  fetchService.analysis();
+  res.send("analysis is processing");
+  res.end();
 
-  console.log(results);
-  res.append("Content-Type", 'text/html; charset=utf-8');
-  res.json(results);
 });
 
 module.exports = router;
